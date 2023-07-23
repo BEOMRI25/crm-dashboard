8dd084de7d915e4167a9b112f4297a6cd314be13
BEOMRI25 <bergman.omri@gmail.com>
2023-07-23 22:32:15 +0300

    Refactored glow container border radius solution

--- src/Components/Glow.js
+++ src/Components/Glow.js
@@ -5,7 +5,7 @@ export default function Glow() {
   const glowContainer = useRef()
   useLayoutEffect(() => {
     const parent = glowContainer.current.parentElement
-    if (parent.style.position === 'static') parent.style.position = 'relative'
+    if (parent.style.position !== 'static') parent.style.position = 'relative'
   }, [])
   return (
     <div className='glow-container' ref={glowContainer}>

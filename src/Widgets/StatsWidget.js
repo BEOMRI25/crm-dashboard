9222da81be68799ae0fa71bdaf700adf523c18bb
Yuval Gal Nachman <yuval@yuvalnachman.design>
2023-07-23 22:19:21 +0300

    resolved glow container issue where the parent element would not set to relative, added tabs to calendar ans stats widgets

--- src/Widgets/StatsWidget.js
+++ src/Widgets/StatsWidget.js
@@ -1,12 +1,27 @@
+import { useState } from 'react'
 import Widget from '../Components/Widget'
 import WidgetContent from '../Components/WidgetContent'
 import WidgetContentNavigation from '../Components/WidgetContentNavigation'
+import Tabs from '../Components/Tabs'
+import Tab from '../Components/Tab'
 
 export default function StatsWidget() {
+  const tabs = [
+    { id: 1, title: 'סה״כ הכנסות' },
+    { id: 2, title: 'ממוצע לקוח' },
+    { id: 3, title: 'ממוצע יומי' },
+  ]
+  const [activeTab, setActiveTab] = useState(tabs[0].title)
   return (
     <Widget id='stats'>
       <WidgetContent>
-        <WidgetContentNavigation></WidgetContentNavigation>
+        <WidgetContentNavigation>
+          <Tabs>
+            {tabs.map(tab => {
+              return <Tab key={tab.id} title={tab.title} active={tab.title === activeTab} onClick={() => setActiveTab(tab.title)} />
+            })}
+          </Tabs>
+        </WidgetContentNavigation>
       </WidgetContent>
     </Widget>
   )
